91697681030d9524800993668ab84169e94f66b9
Suhail Ahmed <arainsuhail451@gmail.com>
2026-07-25 22:36:00 +0500

    feat: Added crud operations

--- filterPractice/src/main/java/com/filterPractice/filterPractice/Controller/StudentController.java
+++ filterPractice/src/main/java/com/filterPractice/filterPractice/Controller/StudentController.java
@@ -6,9 +6,10 @@ import com.filterPractice.filterPractice.Service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.OutputStream;
+import java.util.List;
 
 @Controller
 public class StudentController {
@@ -26,5 +27,44 @@ public class StudentController {
 
         return new ResponseEntity<>(saved,HttpStatus.CREATED);
     }
+
+    @GetMapping("student/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable long id){
+
+        Student myStudent=studentService.getStudent(id);
+
+        return new ResponseEntity<>(myStudent,HttpStatus.OK);
+    }
+
+
+    @GetMapping("student")
+    public ResponseEntity<List<Student>> getALL(){
+       List<Student> ourStudent= studentService.getALL();
+
+        return new ResponseEntity<>(ourStudent,HttpStatus.OK);
+    }
+
+
+    @PutMapping("student/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable long id,@RequestBody Student student){
+
+        Student updatedStudent=studentService.updateStudent(id,student);
+
+        return new ResponseEntity<>(updatedStudent,HttpStatus.CREATED);
+
+
+    }
+
+
+    @DeleteMapping("student/{id}")
+    public ResponseEntity<String>  deletedByID(@PathVariable long id, OutputStream outputStream){
+
+        studentService.deltebyid(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+
+    }
+
+
 
 }
--- filterPractice/src/main/java/com/filterPractice/filterPractice/Service/StudentService.java
+++ filterPractice/src/main/java/com/filterPractice/filterPractice/Service/StudentService.java
@@ -4,7 +4,12 @@ package com.filterPractice.filterPractice.Service;
 import ch.qos.logback.classic.boolex.StubEventEvaluator;
 import com.filterPractice.filterPractice.Entity.Student;
 import com.filterPractice.filterPractice.Repository.StudentRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -23,5 +28,43 @@ public class StudentService {
         return saved;
 
     }
+
+
+    public Student getStudent(Long id){
+
+        Student myStudent=studentRepo.getById(id);
+
+        return myStudent;
+
+    }
+
+
+
+    public List<Student> getALL(){
+
+        List<Student>  myList=studentRepo.findAll();
+        return myList;
+
+
+    }
+
+    public Student updateStudent(long id,Student student){
+
+       Optional<Student> existing =studentRepo.findById(id);
+
+       Student  existingStudent=existing.get();
+       existingStudent.setName(student.getName());
+       existingStudent.setAge(student.getAge());
+
+        return studentRepo.save(existingStudent);
+
+    }
+
+    public void deltebyid(long id){
+
+        studentRepo.deleteById(id);
+
+    }
+
 
 }
