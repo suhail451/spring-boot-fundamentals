6c83c7a65486a7ce4f166c511bd8855106d6b956
Suhail Ahmed <arainsuhail451@gmail.com>
2026-07-27 11:54:19 +0500

    feat: Added a loggin filter

---
+++ filterPractice/src/main/java/com/filterPractice/filterPractice/Filter/LoggingFilter.java
@@ -0,0 +1,37 @@
+package com.filterPractice.filterPractice.Filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class filter implements Filter {
+
+
+    private final HttpServletResponse httpServletResponse;
+
+    public filter(HttpServletResponse httpServletResponse) {
+        this.httpServletResponse = httpServletResponse;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        System.out.println("Logging "+httpRequest.getHttpServletMapping()+"\n"+
+                httpRequest.getMethod()+"\n"+
+                httpRequest.getRequestURI());
+
+        filterChain.doFilter(httpRequest,httpResponse);
+
+    }
+
+
+
+
+}
