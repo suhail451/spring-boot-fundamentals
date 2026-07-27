81f8e9a3dcbbe3ff71b5e6a2dc558bb4b79ec2f7
Suhail Ahmed <arainsuhail451@gmail.com>
2026-07-27 12:28:27 +0500

    feat: added request id in header through filter

---
+++ filterPractice/src/main/java/com/filterPractice/filterPractice/Filter/RequestIDFilter.java
@@ -0,0 +1,27 @@
+package com.filterPractice.filterPractice.Filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class RequestIDFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest=(HttpServletRequest)servletRequest;
+
+        HttpServletResponse httpServletResponse=(HttpServletResponse)servletResponse;
+
+        String requestID= UUID.randomUUID().toString();
+
+        httpServletResponse.setHeader("X-Request-ID",requestID);
+        System.out.println("Request id added in header\n");
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
+
+    }
+}
